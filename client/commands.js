230241647e9fbbcc169ce67c8565ac81fa4a9344
內爾娛樂科技 <halrison@outlook.com>
2019-02-12 22:33:36 +0800

    測試投票功能

--- client/commands.js
+++ client/commands.js
@@ -3,6 +3,10 @@ $(function () {
     var keyword = $("input[name='keyword']");
     var response = $("input[name='response']");
     var table = {};
+    var text1 = $("td:nth-child(2)");
+    var text2 = $("td:nth-child(3)");
+    var button1 = $("td:nth-last-child(2)");
+    var button2 = $("td:last");
     $("table").ready(function () {
         $.ajax({
             url: 'search.php',
@@ -12,76 +16,71 @@ $(function () {
             table = JSON.parse(result);
         });
     });
-    for (var i = 2; i <= table.length; i++) {
-        $("tr:last").append('<tr><td>'+table[i].id+'</td><td>' + table[i].KeyWord + '</td><td>' + table[i].Response + '</td><td><input type="button" name="edit" value="修改"/></td><td><input type="button" name="delete" value="刪除"/></td></tr >');
-        var text1 = $("tr:eq(i)>td:eq(2)");
-        var text2 = $("tr:eq(i)>td:eq(3)");
-        var button1 = $("tr:eq(i)>td:eq(4)");
-        var button2 = $("tr:eq(i)>td:last");
-        $("input[name='edit']")[i].click(function () {
-            text1.replaceWith(keyword).attr('disabled');
-            text2.replaceWith(response);
-            text1.val() = table[i].KeyWord;
-            text2.val() = table[i].Response;
-            $(this).attr('name', 'send');
-            $(this).attr('value', '送出');
-            button2.attr('name', 'cancel');
-            button2.attr('value', '取消');
+    for (var i = 2; i <= table.length+1; i++) {
+        $("tr:last").append('<tr><td>'+table[i].id+'</td><td>' + table[i].KeyWord + '</td><td>' + table[i].Response + '</td><td><input type="button" name="edit" value="修改"/></td><td><input type="button" name="delete" value="刪除"/></td></tr >');        
+        $("input[name='edit']")[i-1].click(function () {
+            text1[i].html(keyword).attr('disabled');
+            text2[i].html(response);
+            text1[i].val() = table[i-1].KeyWord;
+            text2[i].val() = table[i-1].Response;
+            $(this).attr({
+                name: 'send',
+                value: '送出'
+            });
+            button2[i].attr({
+                name: 'cancel',
+                value: '取消'
+            })
         });
-        $("input[name='send']")[i].click(function () {
-            if (text1.val() == '') {
-                error = '關鍵字是必填欄位';
-            }
-            if (text2.val() == '') {
-                error &= '\n回應是必填欄位';
-            }
+        $("input[name='send']")[i-1].click(function () {            
             if (error == '') {
                 $.ajax({
                     url: 'update.php',
                     type: 'POST',
                     data: {
                         item: 'command',
-                        keyword: text1.val(),
-                        response: text2.val()
+                        keyword: text1[i].val(),
+                        response: text2[i].val()
                     }
                 }).done(function (result2) {
-                    text1.replaceWith(result2.KeyWord).removeAttr('disabled');
-                    text2.replaceWith(result2.Response);
-                    $(this).attr('name', 'edit');
-                    $(this).attr('value', '修改');
-                    button2.attr('name', 'delete');
-                    button2.attr('value', '刪除');
+                    text1[i].text(result2.KeyWord).removeAttr('disabled');
+                    text2[i].text(result2.Response);
+                    $(this).attr({
+                        name: 'edit',
+                        value: '修改'
+                    });
+                    button2[i].attr({
+                        name: 'delete',
+                        value: '刪除'
+                    });
                 }).fail(error = '更改回應失敗，請稍後再試');
             }
         });
-        $("input[name='delete']")[i].click(function () {
+        $("input[name='delete']")[i-1].click(function () {
             $.ajax({
                 url: 'delete.php',
                 type: 'POST',
                 data: {
                     item: 'command',
-                    keyword: text1.val(),
-                    response: text2.val()
+                    keyword: text1[i].val(),
+                    response: text2[i].val()
                 }
             }).done($("tr")[i].remove());
         });
-        $("input[name='cancel']")[i].click(function () {
-            text1.replaceWith(table[i].KeyWord).removeAttr('disabled');
-            text2.replaceWith(table[i].Response);
-            button1.attr('name', 'edit');
-            button1.attr('value', '修改');
-            $(this).attr('name', 'delete');
-            $(this).attr('value', '刪除');
+        $("input[name='cancel']")[i-1].click(function () {
+            text1[i].html(table[i - 1].KeyWord).removeAttr('disabled');
+            text2[i].html(table[i - 1].Response);
+            button1.attr({
+                name: 'edit',
+                value: '修改'
+            });
+            $(this).attr({
+                name: 'delete',
+                value: '刪除'
+            });
         });
     }
-    $("form").submit(function (event) {
-        event.preventDefault();
-        if (keyword.val() == '') {
-            error = '關鍵字是必填欄位';
-        }
-        if (response.val() == '') {
-            error &= '\n回應是必填欄位';
-        }
+    $("form").submit(function () {        
         if (error == '') {
             $.ajax({
                 url: 'create.php',
---
+++ client/votes.js
@@ -0,0 +1,48 @@
+﻿$(function () {
+    var topic = $("input[name='topic']").val()
+    ,options = []
+    ,rows = $("tr:gt(0)")
+    ,option =rows.find("td").eq(1)
+    ,button1 = $(".button1")
+    ,button2 =$(".button2")
+    for (var row = 0; row <= rows.length;row++) {
+        let i = 0;
+        button1[row].live("click",function () {
+            i++;
+            if (i % 2 == 0) {
+                button1[row].val( '確定');
+                button2[row].val( '取消');
+                option[row].html("<input name='options' type='text' value='"+ options[i]+"'/>");
+            } else {
+                button1[row].val('修改');
+                button2[row].val('刪除');
+                const j = $.inArray(option[row].text(), options);
+                if (j == -1) { options.push(option[row].text()); }
+                else { options[i] = option[row].text();}
+            }
+        })
+        button2[row].live("click",function () {
+            i++;
+            if (i % 2 == 0) { rows[row].remove(); }
+            else {
+                button2[row].val('刪除');
+                button1[row].val('修改')
+                option[row].text(options[row]);
+            }
+        })
+    }    
+    $("input[name='add']").on("click",function () {
+        $("tbody").append("<tr><td></td><td><input name='options' type='text'/></td><td><input class='button1' type='button' value='確定'/></td><td><input class='button2' type='button' value='取消'/></td></tr>")
+    })
+
+    $("form").submit(function () {
+        $.ajax({
+            url: 'create.php',
+            type: 'POST',
+            data: {
+                topic: topic,
+                options:JSON.stringify(options)
+            }
+        }).fail($("#error").text('新增投票失敗，請稍後再試'))
+    })
+})
