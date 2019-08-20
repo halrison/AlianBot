b9b2c4906c0e7892c9ad1a3c13d5cdc6f4fb36c0
內爾娛樂科技 <halrison@outlook.com>
2019-08-20 22:03:23 +0800

    add vote
    新增投票功能

--- server/logout.php
+++ server/logout.php
@@ -3,5 +3,6 @@
 	mb_internal_encoding('utf-8');
 	session_unset();
 	session_destroy();
+	echo "已登出"
 	header(location:'index.html');
 ?>
---
+++ server/search.php
@@ -0,0 +1,7 @@
+<?php
+	session_start();
+	$item=$_REQUEST['item'];
+	$username=$_SESSION['user']
+	$con=new PDO("myslq:host=fakehost,db=Alianbot","halrison","10240102Nel");
+
+?>
