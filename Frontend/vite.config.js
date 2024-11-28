ee8299534e15b18c8d7af44d33a07eff71eca35d
海力森 <halrison@outlook.com>
2024-11-29 00:43:58 +0800

    變更打包設定
    變更打包時切塊大小的警告

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+	build: {
+		rollupOptions:{
+			output: {		
+				compact: true
+			}
+		},	
+		chunkSizeWarningLimit: 3000
+		
+	}
 });
